syntheticpp/dartruntime

commit 71a94bb48e438d7f486bdf822c520a2c69e5656f

Align start of the page's object area to match OS::kMaxPreferredCodeAlignment.
On ia32 we were aligning it only by 8 bytes which would later negate all our attemps to align code objects by 16 or 32 bytes as the very first code object would be misaligned and the rest would have the same misalignment modulo preferred code alignment.

BUG=

Review URL: https://codereview.chromium.org//13927008

git-svn-id: http://dart.googlecode.com/svn/branches/bleeding_edge/dart@21269 260f80e4-7a28-3924-810f-c04153c831b5

runtime/vm/pages.h
@@ -36,7 +36,7 @@ class HeapPage {
 
   uword object_start() const {
     return (reinterpret_cast<uword>(this) +
-            Utils::RoundUp(sizeof(HeapPage), kObjectAlignment));
+            Utils::RoundUp(sizeof(HeapPage), OS::kMaxPreferredCodeAlignment));
   }
   uword object_end() const {
     return object_end_;

commit 12ea1c0a865c00b21d47e4630aac174d585f5712

Fix bug introduced by r21269.
PageSpace::LargePageSizeFor should not use sizeof(HeapPage) directly because real object start offset is additionally aligned.

BUG=

Review URL: https://codereview.chromium.org//14158003

git-svn-id: http://dart.googlecode.com/svn/branches/bleeding_edge/dart@21276 260f80e4-7a28-3924-810f-c04153c831b5

runtime/vm/pages.cc
@@ -132,7 +132,7 @@ PageSpace::~PageSpace() {
 
 
 intptr_t PageSpace::LargePageSizeFor(intptr_t size) {
-  intptr_t page_size = Utils::RoundUp(size + sizeof(HeapPage),
+  intptr_t page_size = Utils::RoundUp(size + HeapPage::ObjectStartOffset(),
                                       VirtualMemory::PageSize());
   return page_size;
 }

runtime/vm/pages.h
@@ -35,8 +35,7 @@ class HeapPage {
   }
 
   uword object_start() const {
-    return (reinterpret_cast<uword>(this) +
-            Utils::RoundUp(sizeof(HeapPage), OS::kMaxPreferredCodeAlignment));
+    return (reinterpret_cast<uword>(this) + ObjectStartOffset());
   }
   uword object_end() const {
     return object_end_;
@@ -58,6 +57,10 @@ class HeapPage {
   RawObject* FindObject(FindObjectVisitor* visitor) const;
 
   void WriteProtect(bool read_only);
+
+  static intptr_t ObjectStartOffset() {
+    return Utils::RoundUp(sizeof(HeapPage), OS::kMaxPreferredCodeAlignment);
+  }
 
  private:
   void set_object_end(uword val) {